00e71f5443ab5858b953050171ef6ae03c198de5
Luke Michaels <lmichaels@gmail.com>
2017-01-05 17:15:20 -0800

    init

---
+++ wp-content/themes/asleepstanding/woocommerce.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+  <main role="main">
+
+    <section>
+
+      <h1><?php the_title(); ?></h1>
+      <?php woocommerce_content(); ?>
+
+    </section>
+
+  </main>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
